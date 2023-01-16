00afff83efe52e775d7e8723fab7ebc651f25784
Shay Ryan <shay.ryan@outlook.com>
2023-01-16 07:46:53 +0000

    Add extract pages.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # fixpdf
 Clean up PDF file
+
+NNNd - delete page NNN
+NNNrDDD - rotate page NNN by DDD degrees clockwise
+NNNe - extract page NNN
+
+To Do:
+Select input file with file dialog box (tkinter)
+
+Move a page
+56m63 - move page 56 to before page 63
+42m38 - move page 42 to before page 38 
+
+Insert pages from another named file
+28i1 - after page 28 insert page 1 from named file
+Maybe this is merge?
+28i* - after page 28 insert all pages from named file
+There could be more than one insert or merge file
+Merge and Insert should be separate ?
+
+Extract and Rotate
+61er90 - Page 61, extract and rotate 90 CW
+
+delete range
+53-58d - delete pages 53 to 58 inclusive
+
+
--- fixpdf.py
+++ fixpdf.py
@@ -1,30 +1,34 @@
 import re
 from pathlib import Path
 from PyPDF2 import PdfFileReader, PdfWriter
-from PyPDF2 import PdfMerger
 
 # directory
 working_directory = r'Q:\Journal\2022'
 
-# files (input, change-list, output)
-input_file_name = r'2022-11.pdf'
+# files (input, change-list, output, extract)
+input_file_name = r'2022-06.pdf'
 infile = Path(working_directory, input_file_name)
 
 changes_file_name = infile.stem + '_changes.txt'
 changes_file = Path(working_directory, changes_file_name)
 
 output_file_name = infile.stem + '_out' + infile.suffix
-outfile = Path(working_directory, output_file_name)
+out_file = Path(working_directory, output_file_name)
+
+extract_file_name = infile.stem + '_extract' + infile.suffix
+extract_file = Path(working_directory, extract_file_name)
 
 print(f'Input file : {input_file_name}\n'
       f'Output file: {output_file_name}\n'
+      f'Extract file: {extract_file_name}\n'
       f'Change List: {changes_file_name}')
 
 delete_pages = []
 rotate_pages = {}    # key: page number; value: angle (clockwise)
+extract_pages = []
 
 # pattern for PDF changes
-pattern = re.compile(r'(\d+)([rd])(\d*)', re.IGNORECASE)
+pattern = re.compile(r'(\d+)([rde])(\d*)', re.IGNORECASE)
 
 # Parse changes
 # Human page numbering starts at 1
@@ -40,12 +44,14 @@ with open(changes_file, 'rt') as f:
                     delete_pages.append(int(match.group(1)) - 1)
                 if match.group(2) == 'r':
                     rotate_pages[int(match.group(1)) - 1] = int(match.group(3))
+                if match.group(2) == 'e':
+                    extract_pages.append(int(match.group(1)) - 1)
 
 # summary of changes
 print(f'Summary of changes (Re-indexed to start ay Page 0:')
 print(f'Delete pages: {delete_pages}')
 print(f'Rotate pages: {rotate_pages}')
-
+print(f'Extract pages: {extract_pages}')
 
 # Make the changes
 with open(infile, 'rb') as pdf_file:
@@ -55,15 +61,26 @@ with open(infile, 'rb') as pdf_file:
     number_of_pages = reader.getNumPages()
     print(f'Pages: {number_of_pages}')
     writer = PdfWriter()
-
+    extract_writer = PdfWriter()
 
     # page numbers are 0 indexed
     for page_num in range(number_of_pages):
-        if page_num not in delete_pages:
+        if page_num in delete_pages:
+            # skip delete pages
+            continue
+        elif page_num in extract_pages:
+            # extract page
+            pdf_page = reader.getPage(page_num)
+            extract_writer.addPage(pdf_page)
+        else:
+            # not delete, not extract. Keep and rotate if needed
             pdf_page = reader.getPage(page_num)
             if page_num in rotate_pages:
                 pdf_page.rotateClockwise(rotate_pages[page_num])
             writer.addPage(pdf_page)
 
-    with open(outfile, 'wb') as pdf_out_file:
+    with open(out_file, 'wb') as pdf_out_file:
         writer.write(pdf_out_file)
+
+    with open(extract_file, 'wb') as pdf_out_file:
+        extract_writer.write(pdf_out_file)
